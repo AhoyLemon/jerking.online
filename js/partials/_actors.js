047a25f3a4c68b9be5443ccaf8111caa7915cb43
Lemon <lemon@hirelemon.com>
2016-10-18 16:03:43 -0500

    Just sketching out an idea.
    ...for now.

---
+++ js/partials/_actors.js
@@ -0,0 +1,33 @@
+// jshint -W098
+var hairColors = [
+  "brunette",
+  "blonde",
+  "redhead",
+  "bald"
+];
+
+var races = [
+  "white",
+  "black",
+  "asian",
+  "hispanic",
+  "jewish",
+];
+
+var actorTags = [
+  "short",
+  "glasses",
+  "chubby",
+  "hairy",
+  "giant",
+  "goth",
+];
+
+var names = {
+  female: [
+  
+  ],
+  male: [
+  
+  ]
+};
---
+++ js/pr0n.js
@@ -0,0 +1,32 @@
+// jshint -W117
+//@prepros-append partials/_titles.js
+
+function numberWithCommas(x) {
+    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+}
+
+var topMovies = [];
+
+$(document).ready(function() {
+  
+  console.log('there are '+pornTitles.length+' titles')
+  var i = 0;
+  while (i < 20) {
+    var r = Math.floor((Math.random() * pornTitles.length));
+    if (topMovies.indexOf(pornTitles[r]) == -1) {
+      topMovies.push(pornTitles[r]);
+      i++;
+    }
+  }
+  var g = Math.floor(Math.random() * (60000 - 50000)) + 50000;
+  var p;
+  var s;
+  var d;
+  $.each( topMovies, function(key, value) {
+    p = ((Math.random() * (99.999 - 88.8888)) + 88) / 100;
+    g = parseInt(g * p);
+    s = Math.floor((Math.random() * 100) + 1);
+    d = moment().subtract(s, 'days').format('l');
+    $('#PornTitles').append('<tr><td>'+(key+1)+'</td><td>'+value+'</td><td><span class="s">$</span>'+numberWithCommas(g)+'</td><td>'+d+'</td></tr>');
+  });
+});

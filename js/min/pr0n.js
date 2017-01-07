05206ba620dd67eeb25d218df93db72a93cb7356
Lemon <AhoyLemon@users.noreply.github.com>
2017-01-07 01:18:46 -0600

    without the word "porn".

--- js/min/pr0n.js
+++ js/min/pr0n.js
@@ -920,7 +920,7 @@ $('header .logo').hover(function() {
 $('.top-porn').on('click', 'td.title', function() {
   var title = $(this).text();
   var rank = $(this).siblings('.rank').text();
-  var intentText = encodeURI("Number "+rank+" Porn for "+currentDay.short+": "+title);
+  var intentText = encodeURI("Number "+rank+" for "+currentDay.short+": "+title);
   var intentURL = "https://twitter.com/intent/tweet?text="+intentText+'%0ahttp://jerking.online';
   sendEvent('share title', 'Twitter', title);
   window.open(intentURL, "popupWindow", "width=550,height=340");
